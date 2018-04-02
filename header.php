cc74e400b092464e109e3ee822d21956a5ab9e53
patt0108 <patt0108@algonquinlive.com>
2018-04-02 13:50:47 -0400

    Somehow missed the header
    header.php

--- header.php
+++ header.php
@@ -3,7 +3,9 @@
     <link rel="stylesheet" href="css/style.css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>	
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+	<link rel="stylesheet" type="text/css" href="./css/ourstyle.css">
+
 </head>
 
 <body class="bg-success">
@@ -20,7 +22,29 @@ if ($db->connect_error) {
 
 if (isset($_SESSION['userid'])) {
 ?>
-<nav id="top">| <a href="<?=$_SESSION['usertype']?>_cp.php">Control Panel</a> | <a href="logout.php">Log out</a> <?=$_SESSION['userid']?> |</nav>
+	<nav class="navbar navbar-inverse navbar-fixed-top navbar-default" role="navigation">
+		<div class="container">
+			<!-- Brand and toggle get grouped for better mobile display -->
+			<div class="navbar-header">
+				<a href="#" class="navbar-left"><img class="img-rounded" src="./img/gonq.png"></a>
+			</div><!-- Collect the nav links, forms, and other content for toggling -->
+			<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+				<ul class="nav navbar-nav">
+					<li>
+						<a class="nav-link"><?=$_SESSION['realname']?></a>
+					</li>
+				</ul>
+				<ul class="nav navbar-nav navbar-right">
+					<li>
+						<a class="nav-link" href="<?=$_SESSION['usertype']?>_cp.php">Control Panel</a>
+					</li>
+					<li>
+						<a href="logout.php" class="nav-link">Log out</a>
+					</li>
+				</ul>
+			</div>
+		</div>
+	</nav>
 <?php
 }
 ?>
